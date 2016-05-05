bb0483facf02e940d92239f316824c92f662c7f6
Dhiraj Prasad <maildhirajprasad@gmail.com>
2016-05-05 15:25:26 +0530

    adding files

---
+++ project/simplecore/src/main/java/org/simplecore/App.java
@@ -0,0 +1,13 @@
+package org.simplecore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
