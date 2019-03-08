c97b05eea442bb06fae74ede3904c2316f7ff0f9
anjali <anjali@DHIRAJ>
2019-03-08 22:14:30 +0530

    modified according to hibernate 4 specification

--- project/webappDAO/src/main/java/com/webapp/dao/util/HibernateUtil.java
+++ project/webappDAO/src/main/java/com/webapp/dao/util/HibernateUtil.java
@@ -1,10 +1,17 @@
 package com.webapp.dao.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+/**
+ * 
+ * @author dhiraj
+ *
+ */
 @Component
 public class HibernateUtil {
 	 
@@ -14,11 +21,27 @@ public class HibernateUtil {
 	}
 	@Autowired
 	private static SessionFactory sessionFactory;
+/**
+ * According to Hibernate 4.0 API docs, the Configuration class’ buildSessionFactory() method is deprecated 
+ * and it recommends developers to use the buildSessionFactory(ServiceRegistry) instead. 
+ * Here is the new recommended code snippet that builds the SessionFactorybased on a ServiceRegistry 
+ * and obtains the Session:
+ * @return
+ */
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration().configure();
+            ServiceRegistryBuilder registry = new ServiceRegistryBuilder();
+            registry.applySettings(configuration.getProperties());
+            ServiceRegistry serviceRegistry = registry.buildServiceRegistry();
+             
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);           
+        }
+         
+        return sessionFactory;
+    }
+   
 
-
-	    public static SessionFactory getSessionFactory() {
-	        return sessionFactory;
-	    }
 	    
 	    
 }
